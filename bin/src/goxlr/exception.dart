48fe1fd8dc161af813069ca8d8cad0ed4f31d034
Dominik Marcak <dominik.marcak@gmail.com>
2022-02-17 01:12:19 +0100

    feat: add ProfileNotFound exception

--- bin/src/goxlr/exception.dart
+++ bin/src/goxlr/exception.dart
@@ -2,3 +2,12 @@ class GoXLRUnavailable implements Exception {
   @override
   String toString() => 'GoXLR is unavailable.';
 }
+
+class ProfileNotFound implements Exception {
+  final String name;
+
+  ProfileNotFound(this.name);
+
+  @override
+  String toString() => 'Profile $name not found.';
+}
